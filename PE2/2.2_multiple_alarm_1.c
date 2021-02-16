3c179c0d2b07c7558558f01f77921cb779d9b9c4
Jonas Forsberg <47564158+Eksplosjonas@users.noreply.github.com>
2021-02-16 20:38:56 +0100

    Update 2.2_multiple_alarm_1.c

--- PE2/2.2_multiple_alarm_1.c
+++ PE2/2.2_multiple_alarm_1.c
@@ -5,18 +5,18 @@
      int i=1;
 
      while (i != -1){
-     printf("%s", "Enter a number: ");
-     scanf(" %d", &i);
-     int pid = fork();
+          printf("%s", "Enter a number: ");
+          scanf(" %d", &i);
+          int pid = fork();
 
-     if (pid == 0) {    //if in child process
-         sleep(i);
-         printf("Alarm is ringing in the child with child process ID = %d!\nEnter a number: ",getpid());
-         exit(1);
-     }
-     else {             //if in parent process
-         printf("The newly created alarm has child process ID = %d and is set go off in %d seconds\n",pid,i);
-     }
+          if (pid == 0) {    //if in child process
+              sleep(i);
+              printf("Alarm is ringing in the child with child process ID = %d!\nEnter a number: ",getpid());
+              exit(1);
+          }
+          else {             //if in parent process
+              printf("The newly created alarm has child process ID = %d and is set go off in %d seconds\n",pid,i);
+          }
 
      }
      return 0;
